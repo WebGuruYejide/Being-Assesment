910be99433cc3282103401cd06a744a7464f96a9
Yejide S. N. Travis <Webguru@Yejide.com>
2021-12-07 21:12:43 -0500

    uploaded python project
    Being Assessment Session 2 Python project

---
+++ Being Assesment.py
@@ -0,0 +1,67 @@
+print('Peaceful Greetings! Let us do a Quick State of Being Assessment')
+answer=input('Are you ready to consider? (yes/no):  ')
+
+if answer.lower()=='yes':
+    answer=input('Question 1: Are you at peace, personally, in your life?  ')
+    if answer.lower()=='yes':
+        print('Go You!! You are in prime postion to help someone else.  ')
+    else:
+        print('Consider that you could: Be Still. Go within. Listen. And then, act only on the word heard.')
+ 
+    answer=input('Question 2: Do you think about what you think about yourself? ')
+    if answer.lower()=='yes':
+        print('Great!! That leaves you with the time to find a way to serve in your community.')
+    else:
+        print('Consider that you could: Be Conscious of your thoughts, words and behaviors; and the degree to which they create peace in your life.')
+ 
+    answer=input('Question 3: Honestly, does how you think about yourself work in your best interest?  ')
+    if answer.lower()=='yes':
+        print('Awesome!! What unique gift of yours are you going to share with the world?')
+    else:
+        print('Consider that you could: Be Honest with yourself, as to whether your thoughts, words and behaviors serve you.')
+
+    answer=input('Question 4: Do you treat yourself as well as you treat others whom you love?  ')
+    if answer.lower()=='yes':
+        print('Good for you!!! Who, in the community, are you going to help, be, do or have better?')
+    else:
+        print('Consider that you could: Be Kind to yourself, you deserve it. Monitor your words and judgements about You to You; Be compassionate.')
+
+    answer=input('Question 5: Are the choices you are making for your life working for you?  ')
+    if answer.lower()=='yes':
+        print('Wonderful!! How will you choose to be of service in your community?')
+    else:
+        print('Consider that you could: Be Responsible for changing that which does not serve you and enhancing that which does and promotes your personal peace.')
+
+    answer=input('Question 6: Are you aware of how awesome you are, just being you?  ')
+    if answer.lower()=='yes':
+        print('Terrific!! Share your greatness with the world around you, through service.')
+    else:
+        print('Consider that you could: Stand in your Divine greatness and Be your best.')
+
+    answer=input('Question 7: Are you aware of the power your personal intent?  ')
+    if answer.lower()=='yes':
+        print('Congrats on knowing your greatness!! How are you going to share your greatness, this week?')
+    else:
+        print('Consider: When you change the way you look at things, the things you look at change. The way you perceive things is an extremely powerful tool that will allow you to fully bring the power of intention into your life.')
+
+    answer=input('Question 8: Do you know what you have to offer to the world?  ')
+    if answer.lower()=='yes':
+        print('What a blessing You are!!! The world needs your energy.')
+    else:
+        print('Consider: "How wonderful it is that nobody need wait a single moment before starting to improve the world." – Anne Frank')
+
+    answer=input('Question 9: Don’t you hate it when someone answers their own questions?  ')
+    if answer.lower()=='yes':
+        print('I do.')
+    else:
+        print('Consider: “The best way to find yourself is to lose yourself in the service of others.” – Gandhi')
+
+    answer=input('Question 10: Do you plan to be of service, in your community, within the next seven days?  ')
+    if answer.lower()=='yes':
+        print('Yeeeessss!!! “Start where you are. Use what you have. Do what you can.” – Arthur Ashe')
+    else:
+        print('Consider: “The quality of your life will be determined by the quality of your contribution. When you work to improve the lives of others, your life improves automatically.” – Kurek Ashley  ')
+ 
+
+print('Thank you for considering this Personal Peace Process Assesment!')
+print('Blessed Be!')
